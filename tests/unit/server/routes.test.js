bd003be47ec925e43df490a7182b275916e461d3
Manfrin92 <manfredd_10@hotmail.com>
2022-03-15 00:23:41 -0300

    feat: test config

---
+++ tests/unit/server/routes.test.js
@@ -0,0 +1,12 @@
+import { jest, expect, describe, test, beforeEach } from "@jest/globals";
+
+describe("#Routes - test site for api response", () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+    jest.clearAllMocks();
+  });
+
+  test("my test", () => {
+    expect(true).toBeTruthy();
+  });
+});
