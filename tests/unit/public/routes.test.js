c41cca754a8b72ecfdf4b588bf315db4535732d8
Manfrin92 <manfredd_10@hotmail.com>
2022-03-16 16:55:21 -0300

    feat: tests

--- tests/unit/public/routes.test.js
+++ tests/unit/public/routes.test.js
@@ -1,6 +1,11 @@
 import { jest, expect, describe, test } from "@jest/globals";
 
 describe("#Routes", () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+    jest.clearAllMocks();
+  });
+
   test("my test", () => {
     expect(true).toBeTruthy();
   });
--- tests/unit/server/routes.test.js
+++ tests/unit/server/routes.test.js
@@ -1,12 +1,79 @@
 import { jest, expect, describe, test, beforeEach } from "@jest/globals";
+import config from "../../../server/config.js";
+import { Controller } from "../../../server/controller.js";
+import { handler } from "../../../server/routes.js";
+import TestUtil from "../_util/testUtil.js";
+
+const {
+  pages: { controllerHTML, homeHTML },
+  location,
+} = config;
 
 describe("#Routes - test site for api response", () => {
   beforeEach(() => {
-    jest.resetAllMocks();
+    jest.restoreAllMocks();
     jest.clearAllMocks();
   });
 
-  test("my test", () => {
-    expect(true).toBeTruthy();
+  test("GET / should redirect to home page", async () => {
+    const params = TestUtil.defaultHandleParams();
+
+    params.request.method = "GET";
+    params.request.url = "/";
+
+    await handler(...params.values());
+
+    expect(params.response.end).toHaveBeenCalled();
+    expect(params.response.writeHead).toHaveBeenCalledWith(302, {
+      Location: location.home,
+    });
+  });
+
+  test(`GET /contoller should respond with ${controllerHTML} file stream`, async () => {
+    const params = TestUtil.defaultHandleParams();
+
+    params.request.method = "GET";
+    params.request.url = "/controller";
+
+    const mockFileStream = TestUtil.generateReadableStream(["data"]);
+
+    jest
+      .spyOn(Controller.prototype, Controller.prototype.getFileStream.name)
+      .mockResolvedValue({
+        stream: mockFileStream,
+      });
+
+    jest.spyOn(mockFileStream, "pipe").mockReturnValue();
+
+    await handler(...params.values());
+
+    expect(Controller.prototype.getFileStream).toHaveBeenCalledWith(
+      controllerHTML
+    );
+    expect(mockFileStream.pipe).toHaveBeenCalledWith(params.response);
+  });
+
+  test.todo(
+    "GET /unknown given an inexistent route it should respond with 404"
+  );
+
+  test("GET /file.ext should respond with file stream", async () => {
+    const params = TestUtil.defaultHandleParams();
+
+    params.request.method = "GET";
+    params.request.url = "/controller";
+
+    const mockFileStream = TestUtil.generateReadableStream(["data"]);
+
+    jest
+      .spyOn(Controller.prototype, Controller.prototype.getFileStream.name)
+      .mockResolvedValue({
+        stream: mockFileStream,
+      });
+  });
+
+  describe("exceptions", () => {
+    test.todo("Given inexistent file it should respond with 404");
+    test.todo("Given an error it should respond with 500");
   });
 });
